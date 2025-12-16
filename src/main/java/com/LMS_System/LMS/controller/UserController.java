1c8f15d856ae5d3e094cd425268b938f33afcb5a
Ahmed Yasser <ahmedanani118@gmail.com>
2025-12-16 16:52:33 +0200

    updates

--- src/main/java/com/LMS_System/LMS/controller/UserController.java
+++ src/main/java/com/LMS_System/LMS/controller/UserController.java
@@ -1,17 +1,22 @@
 package com.LMS_System.LMS.controller;
 
 import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @NoArgsConstructor
 @RequestMapping("/user")
 public class UserController {
     @GetMapping
-    public ResponseEntity<String> profile(){
-        return ResponseEntity.ok("You are authenticated with JWT");
+    public ResponseEntity<Map<String,String>> profile(){
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(Map.of("message","You are authenticated with JWT"));
     }
 }

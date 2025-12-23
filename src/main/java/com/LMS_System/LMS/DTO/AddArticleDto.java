f16a03c482960cff3c1808712157237ea0e011be
Ahmed Yasser <ahmedanani118@gmail.com>
2025-12-23 23:17:30 +0200

    add article

---
+++ src/main/java/com/LMS_System/LMS/DTO/AddArticleDto.java
@@ -0,0 +1,26 @@
+package com.LMS_System.LMS.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AddArticleDto {
+
+
+    private String name;
+
+
+    private String description;
+
+
+    private LocalDate creationTime;
+
+    private int contentId;
+}
---
+++ src/main/java/com/LMS_System/LMS/service/ArticleService.java
@@ -0,0 +1,50 @@
+package com.LMS_System.LMS.service;
+
+import com.LMS_System.LMS.DTO.AddArticleDto;
+import com.LMS_System.LMS.DTO.GetArticleDto;
+import com.LMS_System.LMS.model.Article;
+import com.LMS_System.LMS.model.Content;
+import com.LMS_System.LMS.repository.ArticleRepository;
+import com.LMS_System.LMS.repository.ContentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class ArticleService {
+
+    @Autowired
+    private ArticleRepository articleRepository;
+
+    @Autowired
+    private ContentRepository contentRepository;
+
+    public ResponseEntity<Map<String,String>> addArticle(AddArticleDto addArticleDto){
+        Content content=contentRepository.findById(addArticleDto.getContentId()).orElse(null);
+
+        if(content==null){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(Map.of("message","Content not found."));
+        }
+        Article article=new Article();
+        article.setDescription(addArticleDto.getDescription());
+        article.setName(addArticleDto.getName());
+        article.setCreationTime(addArticleDto.getCreationTime());
+        article.setContent(content);
+        articleRepository.save(article);
+
+        return  ResponseEntity.status(HttpStatus.OK)
+                .body(Map.of("message","Added successfully."));
+    }
+
+//    public List<?> getArticle(GetArticleDto getArticleDto){
+//
+//        Article article=articleRepository.findById(getArticleDto.getArticleId()).orElse(null);
+//    return List.of();
+//    }
+
+}
